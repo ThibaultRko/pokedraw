abe19aeb42d2a24c100da1e9c999c3157ebfafb0
thibault hercod <hercod.thibault@gmail.com>
2023-09-15 15:50:57 +0200

    ajout forms profil

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const mysql = require('mysql2');
 
 const app = express();
-const port = 3000;
+const port = 3306;
+
+app.use(express.json()); // Pour pouvoir analyser les données JSON des requêtes POST
 
 // Configuration de la connexion à la base de données
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'pokedb',
+  host: process.env.BD_HOST,
+  user: process.envDB_USER,
+  password: process.env_DB_PASSWORD,
+  database: process.env_DB_DATABASE,
 });
 
 // Connexion à la base de données
@@ -21,7 +23,35 @@ db.connect((err) => {
   }
 });
 
+// Endpoint GET
+app.get('/api/data', (req, res) => {
+  // Ici, tu peux exécuter une requête SQL pour récupérer des données de la base de données
+  // Puis, tu renvoies les données en réponse
+  res.json({ message: 'Données récupérées avec succès' });
+});
+
+// Endpoint POST
+app.post('/api/data', (req, res) => {
+  const { key } = req.body; // Suppose que le corps de la requête contient des données au format JSON
+
+  // Ici, tu peux insérer les données dans la base de données en utilisant une requête SQL
+  // Assure-toi de valider et d'échapper les données correctement pour éviter les injections SQL
+
+  res.json({ message: 'Données insérées avec succès' });
+});
+
+// Endpoint DELETE
+app.delete('/api/data/:id', (req, res) => {
+  const { id } = req.params;
+
+  // Ici, tu peux supprimer les données correspondant à l'ID spécifié dans la base de données
+  // Encore une fois, assure-toi de valider et d'échapper les données correctement
+
+  res.json({ message: `Donnée avec l'ID ${id} supprimée avec succès` });
+});
+
 // Démarrez le serveur Express
 app.listen(port, () => {
   console.log(`Serveur en écoute sur le port ${port}`);
 });
+
